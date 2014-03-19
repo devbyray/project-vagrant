50b6257c9cc3ca77828bc27a09eefd6e779920ab
Raymon <raymon@internetstrategiecoaching.nl>
2014-03-19 16:52:45 +0100

    Edit Session controller & Vagrant file & Bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -6,5 +6,7 @@ add-apt-repository -y ppa:chris-lea/node.js
 apt-get update
 apt-get install -y nodejs
 npm install -g sails
+apt-get install -y build-essential
+npm install -g bcrypt
 sudo apt-get install ruby
 sudo apt-get install rubygems
