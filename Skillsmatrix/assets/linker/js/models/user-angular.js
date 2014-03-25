ab70cb5981cb87c907f1201bbf7009a9823f9d48
Raymon Schouwenaar <raymon@raymonschouwenaar.nl>
2014-03-25 17:01:06 +0100

    Added search function with angular

--- Skillsmatrix/assets/linker/js/models/user-angular.js
+++ Skillsmatrix/assets/linker/js/models/user-angular.js
@@ -8,10 +8,23 @@ userApp.controller('UserController', function($scope, $http) {
     $http.get('/user/find').success(function(users) {
             $scope.loaded = true;
             $scope.users = users;
-            console.log(users);
     }).error(function(err) {
         // Alert if there's an error
-        alert(err);
+        console.log(err);
     });
 
+});
+
+userApp.config(function($sceDelegateProvider) {
+     $sceDelegateProvider.resourceUrlWhitelist([
+       // Allow same origin resource loads.
+       'self',
+       // Allow loading from our assets domain.  Notice the difference between * and **.
+       // 'http://srv*.assets.example.com/**'
+    ]);
+
+     // The blacklist overrides the whitelist so the open redirect here is blocked.
+     $sceDelegateProvider.resourceUrlBlacklist([
+        'http://myapp.example.com/clickThru**',
+    ]);
 });
